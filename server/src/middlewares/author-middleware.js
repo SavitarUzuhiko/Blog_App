a0f0dfe3862088c25c80ff7a52aee41337954c2a
Savitar_Uzuhiko <141311341+SavitarUzuhiko@users.noreply.github.com>
2025-07-22 19:40:20 +0500

    Update author-middleware.js

--- server/src/middlewares/author-middleware.js
+++ server/src/middlewares/author-middleware.js
@@ -1,1 +1,10 @@
 
+const model = require('../model/blog.model');
+
+module.exports = async function (req,res,next) {
+  const post = await model.findById(req.params.blogId);
+  const authorId = req.user.id;
+
+  if(post.author !== authorId) throw new HttpException(403, 'You are not authorized to update this post');
+  next()
+}
