47e98718b56132681bab5a0c36050b87c6797451
Savitar_Uzuhiko <141311341+SavitarUzuhiko@users.noreply.github.com>
2025-07-22 19:38:18 +0500

    Update blog.route.js

--- server/src/routes/blog.route.js
+++ server/src/routes/blog.route.js
@@ -1,14 +1,15 @@
 const blogController = require("../controllers/blog.controller.js");
-const authMiddleware = require("../middlewares/auth-middleware.js");
+const authMiddleware = require("../middlewares/auth.middleware.js");
+const authorMiddleware = require("../middlewares/author.middleware.js");
 const upload = require("../utils/upload.js");
 const { expressValidate } = require("../validators/index.js");
 
 const router = require("express").Router();
 
-router.post('/add', upload.single("imageFile") , expressValidate , blogController.addPost);
-// router.post('/add', authMiddleware, upload.single("imageFile") , expressValidate , blogController.addPost);
 router.get('/all',  blogController.getAllPosts);
-router.put('/update/:blogId',  authMiddleware , upload.single("blogImage") , expressValidate , blogController.updatePost);
-router.delete('/delete/:blogId', expressValidate , blogController.deletePost);
+router.get('/get-one/:blogId',  blogController.getOne);
+router.post('/add', authMiddleware, upload.single("imageFile") , expressValidate , blogController.addPost);
+router.put('/update/:blogId',  authMiddleware , authorMiddleware , upload.single("blogImage") , expressValidate , blogController.updatePost);
+router.delete('/delete/:blogId', authMiddleware, authorMiddleware, expressValidate , blogController.deletePost);
 
 module.exports = router;
